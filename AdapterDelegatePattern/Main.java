f08484d2176daff40c252ee9a63773c8e2090dd4
kouta1612 <kouta1612world69@gmail.com>
2021-05-02 12:20:00 +0900

    AdapterDelegatePattern

---
+++ AdapterDelegatePattern/Main.java
@@ -0,0 +1,9 @@
+package AdapterDelegatePattern;
+
+public class Main {
+    public static void main(String[] args) {
+        Print print = new PrintBanner("Hello");
+        print.printWeak();
+        print.printStrong();
+    }
+}
---
+++ AdapterDelegatePattern/PrintBanner.java
@@ -0,0 +1,17 @@
+package AdapterDelegatePattern;
+
+public class PrintBanner extends Print {
+    private Banner banner;
+
+    public PrintBanner(String string) {
+        this.banner = new Banner(string);
+    }
+
+    public void printWeak() {
+        this.banner.showWithParen();
+    }
+
+    public void printStrong() {
+        this.banner.showWithAster();
+    }
+}
