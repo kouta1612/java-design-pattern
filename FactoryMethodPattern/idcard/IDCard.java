9e72199737eaadbed0ba2fe4d2a49115dcdcd002
kouta1612 <kouta1612world69@gmail.com>
2021-05-02 16:21:24 +0900

    問題4-2実装

--- FactoryMethodPattern/idcard/IDCard.java
+++ FactoryMethodPattern/idcard/IDCard.java
@@ -3,18 +3,24 @@ package FactoryMethodPattern.idcard;
 import FactoryMethodPattern.framework.Product;
 
 public class IDCard extends Product {
+    private int id;
     private String owner;
 
-    IDCard(String owner) {
-        System.out.println(owner + "のカードを作ります。");
+    IDCard(String owner, int id) {
+        System.out.println(owner + "(" + id + ")のカードを作ります。");
         this.owner = owner;
+        this.id = id;
     }
 
     public void use() {
-        System.out.println(this.owner + "のカードを使います。");
+        System.out.println(this.owner + "(" + id + ")のカードを使います。");
     }
 
     public String getOwner() {
         return this.owner;
+    }
+
+    public int getId() {
+        return this.id;
     }
 }
--- FactoryMethodPattern/idcard/IDCardFactory.java
+++ FactoryMethodPattern/idcard/IDCardFactory.java
@@ -1,23 +1,28 @@
 package FactoryMethodPattern.idcard;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Hashtable;
 import java.util.List;
+import java.util.Map;
 
 import FactoryMethodPattern.framework.Factory;
 import FactoryMethodPattern.framework.Product;
 
 public class IDCardFactory extends Factory {
-    private List owners = new ArrayList();
+    private HashMap map = new HashMap();
+    private int id = 100;
 
     protected Product createProduct(String owner) {
-        return new IDCard(owner);
+        return new IDCard(owner, this.id++);
     }
 
     protected void registerProduct(Product product) {
-        this.owners.add(((IDCard) product).getOwner());
+        IDCard idCard = (IDCard) product;
+        this.map.put(Integer.valueOf(idCard.getId()), idCard.getOwner());
     }
 
-    public List getOwners() {
-        return this.owners;
+    public HashMap getMaps() {
+        return this.map;
     }
 }
